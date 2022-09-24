a2136f3a1e72ef392f45c9ef44090d9d50767baa
Moshaheb Hossain <104467902+moshahebhossain@users.noreply.github.com>
2022-09-24 19:57:38 +0600

    Add files via upload

---
+++ C#/Arraysincsharp/Arraysincsharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Arraysincsharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] EvenNumbers = new int[3];
+
+            EvenNumbers[0] = 0;
+            EvenNumbers[1] = 2;
+            EvenNumbers[2] = 4;
+            EvenNumbers[3] = 6;
+
+            Console.WriteLine(EvenNumbers[1]);
+            
+        }
+    }
+}
