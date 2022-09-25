cc01c3332a87d136a77809b9fb23d3693a153453
Moshaheb Hossain <104467902+moshahebhossain@users.noreply.github.com>
2022-09-25 19:19:29 +0600

    Add files via upload

---
+++ whileloopinCsharp/whileloopinCsharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace whileloopinCsharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter your target?");
+            int userTarget = int.Parse(Console.ReadLine());
+
+            int Start = 0;
+            while (Start <= userTarget);
+            {
+                Console.WriteLine(Start);
+                Start = Start + 2;
+            }
+        }
+    }
+}
