c56e8e579ed800fa6e8fb757c73fabe605bebb4e
Moshaheb Hossain <104467902+moshahebhossain@users.noreply.github.com>
2022-10-02 00:10:41 +0600

    Add files via upload

---
+++ dowhileloopinCsharp/dowhileloopinCsharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace dowhileloopinCsharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter your target?");
+            int userTarget = int.Parse(Console.ReadLine());
+
+            int Start = 0;
+            while (Start <= userTarget)
+            {
+                Console.WriteLine(Start + " ");
+                Start = Start + 2;
+            }
+
+            Console.WriteLine("Do you want to continue - Yes or No?");
+
+
+            String UserChoice = Console.ReadLine();
+            if (UserChoice != "Yes" && UserChoice != "no")
+            {
+                Console.WriteLine("Invalid choice, please say yes or no");
+
+            }
+            while (UserChoice != "Yes" && UserChoice != "no") ;
+        } 
+    }
+}
