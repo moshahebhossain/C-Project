8bc7d41441fca94a8b172417b2bbeeb459667edb
Moshaheb Hossain <104467902+moshahebhossain@users.noreply.github.com>
2022-10-02 01:52:10 +0600

    Add files via upload

---
+++ namespace.avi/namespace.avi/Program.cs
@@ -0,0 +1,22 @@
+﻿
+// namespace declaration
+namespace first
+{
+
+    // name_1 namespace members
+    // i.e. class
+    class Geeks_1
+    {
+
+        // function of class Geeks_1
+        public static void display()
+        {
+            // Here System is the namespace
+            // under which Console class is defined
+            // You can avoid writing System with 
+            // the help of "using" keyword discussed
+            // later in this article
+            System.Console.WriteLine("Hello Geeks!");
+
+        }
+    }
