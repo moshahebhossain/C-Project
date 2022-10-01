a238a91db703876bd91a6bd4eafbda1756636ac0
Moshaheb Hossain <104467902+moshahebhossain@users.noreply.github.com>
2022-10-02 01:54:25 +0600

    Add files via upload

---
+++ method overloading/method overloading/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.Method(); //Invoke the 1st Method
+            obj.Method(10); //Invoke the 2nd Method
+            obj.Method("Hello"); //Invoke the 3rd Method
+            obj.Method(10, "Hello"); //Invoke the 4th Method
+            obj.Method("Hello", 10); //Invoke the 5th Method
+            Console.ReadKey();
+        }
+        public void Method()
+        {
+            Console.WriteLine("1st Method");
+        }
+        public void Method(int i)
+        {
+            Console.WriteLine("2nd Method");
+        }
+        public void Method(string s)
+        {
+            Console.WriteLine("3rd Method");
+        }
+        public void Method(int i, string s)
+        {
+            Console.WriteLine("4th Method");
+        }
+        public void Method(string s, int i)
+        {
+            Console.WriteLine("5th Method");
+        }
+    }
+}
