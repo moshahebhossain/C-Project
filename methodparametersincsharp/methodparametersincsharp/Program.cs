8d665169ee21a110f45133ae484d7ec35c696c37
Moshaheb Hossain <104467902+moshahebhossain@users.noreply.github.com>
2022-10-02 01:39:58 +0600

    Add files via upload

---
+++ methodparametersincsharp/methodparametersincsharp/Program.cs
@@ -0,0 +1,23 @@
+﻿// C# program to illustrate the 
+// concept of the named parameters
+using System;
+
+public class GFG
+{
+
+    // addstr contain three parameters
+    public static void addstr(string s1, string s2, string s3)
+    {
+        string result = s1 + s2 + s3;
+        Console.WriteLine("Final string is: " + result);
+    }
+
+    // Main Method
+    static public void Main()
+    {
+        // calling the static method with named 
+        // parameters without any order
+        addstr(s1: "Geeks", s2: "for", s3: "Geeks");
+
+    }
+}
